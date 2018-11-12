df601bb7b80624765f4ba829a8ab96e89c1a6006
Adrian Zieliński <adrianziel24@wp.pl>
2018-11-12 22:26:39 +0100

    Adden basic MovementMenager script.

---
+++ Nameless/Assets/Scripts/MovementMenager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementMenager : MonoBehaviour {
+
+    public float speed;
+    //public Rigidbody2D rb;
+    private Vector3 moveVelocity;
+	void Start () {
+        //rb = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update () {
+        Vector2 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        moveVelocity = moveInput.normalized * speed;
+        transform.Translate(moveVelocity);
+	}
+
+    //private void FixedUpdate()
+    //{
+    //    rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    //}
+}
