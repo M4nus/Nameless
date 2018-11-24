79e4cc392385de1c5e040575079f2cbd39cdd74a
Adrian Zieliński <adrianziel24@wp.pl>
2018-11-24 22:25:07 +0100

    Added shooting mechanism (repair needed)

--- Nameless/Assets/Scripts/MovementManager.cs
+++ Nameless/Assets/Scripts/MovementManager.cs
@@ -1,11 +1,24 @@
 ﻿using UnityEngine;
 
 public class MovementManager : MonoBehaviour
-{     
+{
+    public float angle;
+    public float rotspeed = 10f;
+
 	void Update ()
     {
-        Vector2 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
-        Vector3 moveVelocity = moveInput.normalized * GameSpace.GameManager.instance.playerSpeed * Time.deltaTime;
-        transform.Translate(moveVelocity);
-	}           
+        //Vector2 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        //Vector3 moveVelocity = moveInput.normalized * GameSpace.GameManager.instance.playerSpeed * Time.deltaTime;
+        //transform.Translate(moveVelocity);
+        Quaternion rot = transform.rotation;
+        float z = rot.eulerAngles.z;
+        z -= Input.GetAxisRaw("Horizontal") * rotspeed * Time.deltaTime;
+        rot = Quaternion.Euler(0, 0, z);
+        transform.rotation = rot;
+        Vector3 pos = transform.position;
+        Vector3 velocity = new Vector3(0, Input.GetAxisRaw("Vertical") * 10 * Time.deltaTime);
+        pos += rot * velocity;
+        transform.position = pos;
+
+    }           
 }
---
+++ Nameless/Assets/Scripts/Shooting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    public Transform firePoint;
+    public GameObject bulletPrefeab;
+    ObjectPooler objectPooler;
+	
+	
+    void Start()
+    {
+        objectPooler = ObjectPooler.Instance; 
+    }
+
+	void Update () {
+		if(Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
+	}
+
+    void shoot()
+    {
+        objectPooler.SpawnFromPool("Bullet", firePoint.transform.position, firePoint.transform.rotation);
+        //Instantiate(bulletPrefeab, firePoint.position, firePoint.rotation);
+    }
+}
